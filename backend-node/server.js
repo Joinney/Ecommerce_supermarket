0152e231b4636f8c648fa86279efa119438fe790
Vo Duy Toan <voduyduydlk@gmail.com>
2026-04-25 01:53:55 +0700

    be: fix google trả về trang chủ lan 2

--- backend-node/server.js
+++ backend-node/server.js
@@ -21,6 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000; 
 
 // 1. CẤU HÌNH CORS (Bản fix mạnh tay cho Demi)
