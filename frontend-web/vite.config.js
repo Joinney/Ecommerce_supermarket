4cc458cc99ce0a76b714c81cb322058a3e060b5e
Vo Duy Toan <voduyduydlk@gmail.com>
2026-04-25 19:04:08 +0700

    feat: kết nối  Dockerized cho Demi Mart

--- frontend-web/vite.config.js
+++ frontend-web/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Tương đương với --host, cho phép truy cập từ ngoài
+    port: 5173,
+    // --- THÊM ĐOẠN NÀY VÀO ---
+    allowedHosts: [
+      'host.docker.internal',
+      'localhost'
+    ]
+    // ------------------------
+  }
 })
